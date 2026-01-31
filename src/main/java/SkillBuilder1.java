fb10f5dfbc3807b3ea86962354f3e8e24191ed7a
jmgreer675 <justinmgreer@hotmail.com>
2026-01-31 11:16:07 -0500

    Solved both problems

--- src/main/java/SkillBuilder1.java
+++ src/main/java/SkillBuilder1.java
@@ -9,7 +9,16 @@ import java.util.Scanner;
 public class SkillBuilder1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //TODO : Replace this comment with your code
+        System.out.println("You're Jane's friend!");
+        System.out.println("\"What's your name?\"");
+        String name = input.nextLine();
+        System.out.println("Enter a floating-point number:");
+        double spice = input.nextDouble();
+        double calculation = (4.0/3.0)* Math.pow(2, Math.sqrt(5)/Math.pow(spice, 3));
+        System.out.printf("Well " + name + ", the spice value resulted in %.4f\n", calculation);
+        double intValue = (int)(calculation * 100) / 100.0;
+        System.out.println("And the converted value is " + intValue);
+
     }
 
     public static void calcWallPaint() {
@@ -20,16 +29,20 @@ public class SkillBuilder1 {
 
         // Prompt user to input wall's width
         System.out.println("Enter wall height (feet):");
-        double wallHeight = input.nextDouble();
+        int wallHeight = input.nextInt();
 
         // Prompt user to input wall's width
         System.out.println("Enter wall width (feet):");
-        double wallWidth = input.nextDouble();
+        int wallWidth = input.nextInt();
 
-        // TODO: Calculate and output the wall's area
+        int wallArea = wallWidth * wallHeight;
+        System.out.println("Wall area: " + wallArea + " square feet");
 
-        // TODO: Calculate and output the amount of paint in gallons needed to paint the wall
+        double paintNeeded = wallArea / squareFeetPerGallons;
+        System.out.printf("Paint needed: %.2f gallons\n", paintNeeded);
 
-        // TODO: Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+        int cansNeeded = (int) Math.ceil(paintNeeded / gallonsPerCan);
+        System.out.println("Cans needed: " + cansNeeded + " can(s)");
+
     }
 }
